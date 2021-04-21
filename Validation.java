54e423cfb8a185ffe9c1e050c86c42dee273fa1f
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-21 17:58:41 +0530

    ADD:pincode validation

--- Validation.java
+++ Validation.java
@@ -1,7 +1,27 @@
+import java.util.regex.*;
+
 class Validation
 {
-public static void main(String args[])
+public static boolean isValidPinCode(String pinCode)
 {
-System.out.println("Welcome to validation program");
-}
-}
+String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+
+Pattern p = Pattern.compile(regex);
+
+        if(pinCode == null)
+        {
+                return false;
+        }
+
+        Matcher m = p.matcher(pinCode);
+                return m.matches();
+        }
+
+        public static void main(String args[])
+        {
+                String num1 = args[0];
+                System.out.println(num1 +" : "+ isValidPinCode(num1));
+        }
+        }
+
+
